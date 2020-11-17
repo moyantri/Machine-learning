265d20bed5caa2acd684192cbbbed8b30baabada
Moyantri Koley <51296244+moyantri@users.noreply.github.com>
2020-11-17 08:41:15 +0530

    Create tfData.py

---
+++ tfData.py
@@ -0,0 +1,43 @@
+#The tf.data API enables you to build complex input pipelines from simple, reusable pieces. The tf.data API also makes it possible to handle large amounts of data, read from different data formats, and perform complex transformations.
+
+#The tf.data API introduces a tf.data.Dataset abstraction that represents a sequence of elements, in which each element consists of one or more components. For example, in an image pipeline, an element might be a single training example,
+#with a pair of tensor components representing the image and its label.
+#🎯 Input Pipeline :
+
+#tf.data.TFRecordDataset() is used to create an input pipeline for data stored in TFRecord format .
+
+#🎯 Transformations :
+
+#The Dataset object can be transformed into a new Dataset by chaining method calls on the tf.data.Dataset object .
+#Some of the transformations which can be applied are Dataset.map() , Dataset.batch() , Dataset.shuffle() , Dataset.prefetch() .
+
+def decode_to_tensor(image):
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.cast(image, tf.float32) / 255.0
+    image = tf.reshape(image, [*IMAGE_SIZE, 3])
+    return image
+def read_tfrecord(example, labeled):
+    tfrecord_format = {
+        "image": tf.io.FixedLenFeature([], tf.string),
+        "target": tf.io.FixedLenFeature([], tf.int64)
+    } if labeled else {
+        "image": tf.io.FixedLenFeature([], tf.string),
+        "image_name": tf.io.FixedLenFeature([], tf.string)
+    }
+    example = tf.io.parse_single_example(example, tfrecord_format)
+    image = decode_to_tensor(example['image'])
+    if labeled:
+        label = tf.cast(example['target'], tf.int32)
+        return image, label
+    idnum = example['image_name']
+    return image, idnum
+    def load_dataset(filenames, labeled=True, ordered=False):
+    ignore_order = tf.data.Options()
+    if not ordered:
+        ignore_order.experimental_deterministic = False # disable order, increase speed
+    dataset = tf.data.TFRecordDataset(filenames, num_parallel_reads=AUTOTUNE) # automatically interleaves reads from multiple files
+    dataset = dataset.with_options(ignore_order) # uses data as soon as it streams in, rather than in its original order
+    dataset = dataset.map(partial(read_tfrecord, labeled=labeled), num_parallel_calls=AUTOTUNE)
+    # returns a dataset of (image, label) pairs if labeled=True or (image, id) pairs if labeled=False
+    return dataset
+    
