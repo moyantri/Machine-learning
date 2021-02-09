7b3f5f59793e9726a2e9f3387b5f9637c13b2ba2
Moyantri Koley <51296244+moyantri@users.noreply.github.com>
2021-02-09 12:19:04 +0530

    Update main.py

--- melanoma/main.py
+++ melanoma/main.py
@@ -35,4 +35,43 @@ class SEResNext50_32x4d(nn.Module):
             out, targets.reshape(-1, 1).type_as(out)
         )
         return out, loss
+def train(fold):
+    training_data_path = "/home/moyantri/workspace/melanoma/input/jpeg/train224/"
+    model_path = "/home/moyantri/workspace/melanoma-deep-learning"
+    df = pd.read_csv("train_folds.csv")
+    device = "cuda"
+    epochs = 50
+    train_bs = 32
+    valid_bs = 16
+    mean = (0.485, 0.456, 0.406) #mean
+    std = (0.229, 0.224, 0.225) #standard deviation
 
+    df_train = df[df.kfold != fold].reset_index(drop=True)
+    df_valid = df[df.kfold == fold].reset_index(drop=True)
+    
+    
+    train_aug = albumentations.Compose(
+        [
+            albumentations.Normalize(mean, std, max_pixel_value=255.0, always_apply=True),
+        ]
+    )
+    
+    
+        [
+            albumentations.Normalize(mean, std, max_pixel_value=255.0, always_apply=True),
+        ]
+    )
+ # training albumentations
+    valid_aug = albumentations.Compose(
+        [
+            albumentations.Normalize(mean, std, max_pixel_value=255.0, always_apply=True),
+        ]
+    )
+
+    valid_aug = albumentations.Compose(
+        [
+            albumentations.Normalize(mean, std, max_pixel_value=255.0, always_apply=True),
+        ]
+    )
+    # validation
+    
